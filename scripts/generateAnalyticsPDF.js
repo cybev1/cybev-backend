1e079f0736d2906658f460fa669a6d0dbe3bc81d
cybev1 <cybev.io@gmail.com>
2025-06-18 05:36:34 +0000

    Add files via upload

--- scripts/generateAnalyticsPDF.js
+++ scripts/generateAnalyticsPDF.js
@@ -1,17 +1,30 @@
 
 const fs = require('fs');
-const path = require('path');
-const pdf = require('html-pdf');
+const { PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 
-const generateAnalyticsPDF = (htmlContent, outputPath) => {
-  const options = { format: 'A4', border: '10mm' };
+const generateAnalyticsPDF = async (textContent, outputPath) => {
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage([595, 842]); // A4 size
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const { width, height } = page.getSize();
 
-  return new Promise((resolve, reject) => {
-    pdf.create(htmlContent, options).toFile(outputPath, (err, res) => {
-      if (err) return reject(err);
-      resolve(res);
+  const lines = textContent.split('\n');
+  let y = height - 50;
+
+  for (const line of lines) {
+    page.drawText(line, {
+      x: 50,
+      y,
+      size: 12,
+      font,
+      color: rgb(0, 0, 0)
     });
-  });
+    y -= 20;
+    if (y < 40) break;
+  }
+
+  const pdfBytes = await pdfDoc.save();
+  fs.writeFileSync(outputPath, pdfBytes);
 };
 
 module.exports = generateAnalyticsPDF;
