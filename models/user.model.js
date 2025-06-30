df668cae495f44589852c53da5b8c59b6a4c701b
cybev1 <cybev.io@gmail.com>
2025-06-30 13:06:32 +0000

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: { type: String, required: true, unique: true },
+  name: String,
+  email: { type: String, unique: true },
   password: String,
-  isVerified: { type: Boolean, default: false },
-  verificationToken: String
+  referral: String,
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
