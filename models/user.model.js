669ba95b654ac315ef92d79f06606875e237106e
cybev1 <cybev.io@gmail.com>
2025-06-30 06:17:07 +0000

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -1,21 +1,11 @@
+import mongoose from 'mongoose';
 
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: { type: String },
 }, { timestamps: true });
 
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
-
-module.exports = mongoose.model('User', UserSchema);
+export default mongoose.models.User || mongoose.model('User', userSchema);
