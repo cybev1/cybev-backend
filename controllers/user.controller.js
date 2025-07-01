f7f0908510cc59391087de0c25830ce1ee6c8fca
cybev1 <cybev.io@gmail.com>
2025-07-01 11:33:21 +0000

    Add files via upload

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,26 @@
-exports.getFollowing = (req, res) => {
-  res.json({ following: [] }); // Replace with MongoDB logic later
+const User = require('../models/User');
+
+const getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
 };
+
+const updateUserProfile = async (req, res) => {
+  try {
+    const { name, referral } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { name, referral },
+      { new: true }
+    ).select('-password');
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { getUserInfo, updateUserProfile };
