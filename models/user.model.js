40cbf47e60c65bf367d1c97aa446e0ea730cccf9
cybev1 <cybev.io@gmail.com>
2025-10-14 11:54:06 +0000

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,37 +16,125 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 6
   },
-  referral: {
+  username: {
+    type: String,
+    unique: true,
+    sparse: true,
+    lowercase: true,
+    trim: true
+  },
+  bio: {
+    type: String,
+    maxlength: 500,
+    default: ''
+  },
+  avatar: {
     type: String,
     default: ''
   },
-  // ✨ NEW FIELDS for onboarding
-  hasCompletedOnboarding: {
+  customDomain: {
+    type: String,
+    unique: true,
+    sparse: true,
+    lowercase: true,
+    trim: true
+  },
+  domainVerified: {
     type: Boolean,
     default: false
   },
-  contentType: {
-    type: String,
-    enum: ['blog', 'social', 'both', null],
-    default: null
+  socialLinks: {
+    twitter: String,
+    linkedin: String,
+    github: String,
+    website: String
   },
-  // Timestamps
-  createdAt: {
-    type: Date,
-    default: Date.now
+  preferences: {
+    emailNotifications: {
+      type: Boolean,
+      default: true
+    },
+    newsletterSubscription: {
+      type: Boolean,
+      default: false
+    },
+    theme: {
+      type: String,
+      enum: ['light', 'dark', 'auto'],
+      default: 'auto'
+    }
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  lastLogin: {
+    type: Date
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
   }
 });
 
-// Update timestamp on save
-userSchema.pre('save', function(next) {
-  this.updatedAt = Date.now();
+// Generate username from email if not provided
+userSchema.pre('save', async function(next) {
+  if (!this.username && this.email) {
+    const baseUsername = this.email.split('@')[0].toLowerCase().replace(/[^a-z0-9]/g, '');
+    let username = baseUsername;
+    let counter = 1;
+    
+    while (await mongoose.model('User').findOne({ username })) {
+      username = `${baseUsername}${counter}`;
+      counter++;
+    }
+    
+    this.username = username;
+  }
   next();
 });
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Remove password from JSON response
+userSchema.methods.toJSON = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+// Virtual for profile URL
+userSchema.virtual('profileUrl').get(function() {
+  if (this.customDomain && this.domainVerified) {
+    return `https://${this.customDomain}`;
+  }
+  return `${process.env.APP_URL}/blog/${this.username}`;
+});
+
+// Indexes
+userSchema.index({ email: 1 });
+userSchema.index({ username: 1 });
+userSchema.index({ customDomain: 1 });
 
 module.exports = mongoose.model('User', userSchema);
