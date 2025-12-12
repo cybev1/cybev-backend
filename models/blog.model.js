3b9d38de0435ff3fa7efb210f337939964ac6be1
cybev1 <cybev.io@gmail.com>
2025-12-12 06:22:22 +0000

    Refactor blog model to add new fields and indexes

--- models/blog.model.js
+++ models/blog.model.js
@@ -6,89 +6,93 @@ const blogSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
+  
   content: {
     type: String,
     required: true
   },
-  category: {
+
+  excerpt: {
     type: String,
-    required: true,
-    enum: [
-      'Technology',
-      'Health & Wellness',
-      'Business & Finance',
-      'Lifestyle',
-      'Travel',
-      'Food & Cooking',
-      'Education',
-      'Entertainment',
-      'Science',
-      'Sports',
-      'Fashion & Beauty',
-      'Personal Development',
-      'News & Politics',
-      'Environment',
-      'Other'
-    ]
+    default: ''
   },
+
+  featuredImage: {
+    type: String,
+    default: ''
+  },
+
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+
   authorName: {
     type: String,
     required: true
   },
-  featuredImage: {
+
+  category: {
     type: String,
-    default: ''
+    default: 'general',
+    lowercase: true,
+    trim: true
+    // NO ENUM - Allow any category!
   },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  views: {
-    type: Number,
-    default: 0
-  },
-  readTime: {
-    type: Number,
-    default: 5
-  },
-  featured: {
-    type: Boolean,
-    default: false
-  },
+
   tags: [{
     type: String,
     trim: true
   }],
+
   status: {
     type: String,
     enum: ['draft', 'published', 'archived'],
-    default: 'published'
+    default: 'draft'
+  },
+
+  views: {
+    type: Number,
+    default: 0
+  },
+
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+
+  readTime: {
+    type: Number,
+    default: 5
+  },
+
+  isAIGenerated: {
+    type: Boolean,
+    default: false
+  },
+
+  seo: {
+    metaTitle: String,
+    metaDescription: String,
+    keywords: [String]
   }
-}, {
-  timestamps: true
+
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
-blogSchema.index({ category: 1, createdAt: -1 });
-blogSchema.index({ author: 1 });
-blogSchema.index({ featured: 1 });
-blogSchema.index({ status: 1 });
+// Indexes
+blogSchema.index({ author: 1, createdAt: -1 });
+blogSchema.index({ status: 1, createdAt: -1 });
+blogSchema.index({ category: 1 });
+blogSchema.index({ tags: 1 });
 
+// Virtual for like count
 blogSchema.virtual('likeCount').get(function() {
-  return this.likes.length;
-});
-
-blogSchema.pre('save', function(next) {
-  if (this.isModified('content')) {
-    const wordsPerMinute = 200;
-    const wordCount = this.content.split(/\s+/).length;
-    this.readTime = Math.ceil(wordCount / wordsPerMinute);
-  }
-  next();
+  return this.likes ? this.likes.length : 0;
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
