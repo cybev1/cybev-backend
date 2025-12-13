4c4bb43efc93d13eb621975c2257d1daec4b5de0
cybev1 <cybev.io@gmail.com>
2025-12-13 04:09:31 +0000

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -16,6 +16,8 @@ router.options('*', (req, res) => {
 // Authentication routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 // User Profile
 router.get('/me', verifyToken, async (req, res) => {
