69087149fe5499d9bb041ae4eea6be417fdc4fc6
cybev1 <cybev.io@gmail.com>
2025-12-27 13:44:26 +0000

    Update socket.js

--- socket.js
+++ socket.js
@@ -6,41 +6,58 @@ const socketIo = require('socket.io');
 let io;
 
 function initializeSocket(server) {
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://cybev.io',
+    'https://www.cybev.io',
+    process.env.CLIENT_URL
+  ].filter(Boolean);
+
   io = socketIo(server, {
     cors: {
-      origin: process.env.CLIENT_URL || 'http://localhost:3000',
+      origin: allowedOrigins,
       credentials: true,
       methods: ['GET', 'POST']
     }
   });
 
   io.on('connection', (socket) => {
-    console.log('✅ User connected:', socket.id);
+    console.log('✅ Socket connected:', socket.id);
 
     socket.on('join', (userId) => {
       socket.join(`user:${userId}`);
       console.log(`👤 User ${userId} joined their room`);
     });
 
+    socket.on('leave', (userId) => {
+      socket.leave(`user:${userId}`);
+      console.log(`👋 User ${userId} left their room`);
+    });
+
     socket.on('disconnect', () => {
-      console.log('👋 User disconnected:', socket.id);
+      console.log('👋 Socket disconnected:', socket.id);
     });
   });
 
+  console.log('🔌 Socket.io initialized');
   return io;
 }
 
 function getIO() {
-  if (!io) {
-    throw new Error('Socket.io not initialized');
-  }
   return io;
 }
 
 function emitNotification(userId, notification) {
   if (io) {
     io.to(`user:${userId}`).emit('notification', notification);
+    console.log(`📤 Notification sent to user:${userId}`);
   }
 }
 
-module.exports = { initializeSocket, getIO, emitNotification };
+function emitToUser(userId, event, data) {
+  if (io) {
+    io.to(`user:${userId}`).emit(event, data);
+  }
+}
+
+module.exports = { initializeSocket, getIO, emitNotification, emitToUser };
