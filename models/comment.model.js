9819b8c17247b829c94199c0b8153c4032a39d42
cybev1 <cybev.io@gmail.com>
2026-01-02 11:32:26 +0000

    Update comment.model.js

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,3 +1,8 @@
+// ============================================
+// FILE: models/comment.model.js
+// UPDATED: Added authorName field
+// ============================================
+
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
@@ -5,60 +10,98 @@ const commentSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    maxlength: 1000
+    maxlength: 5000
   },
-  blog: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Blog',
-    required: true,
-    index: true
-  },
-  author: {
+  
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+  
+  // Author name for display (required)
   authorName: {
     type: String,
-    required: true
+    required: true,
+    default: 'Anonymous'
   },
+  
+  // Author avatar URL (optional)
+  authorAvatar: {
+    type: String,
+    default: ''
+  },
+  
+  // Reference to blog (if commenting on blog)
+  blog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+    default: null
+  },
+  
+  // Reference to post (if commenting on post)
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    default: null
+  },
+  
+  // Parent comment (for replies/threads)
   parentComment: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Comment',
     default: null
   },
+  
+  // Likes on comment
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }],
+  
+  // Reply comments
+  replies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  
+  // Is edited
   isEdited: {
     type: Boolean,
     default: false
   },
+  
+  // Soft delete
   isDeleted: {
     type: Boolean,
     default: false
   }
-}, {
-  timestamps: true
-});
+  
+}, { timestamps: true });
 
-// Index for efficient queries
+// Indexes
 commentSchema.index({ blog: 1, createdAt: -1 });
-commentSchema.index({ author: 1 });
+commentSchema.index({ post: 1, createdAt: -1 });
+commentSchema.index({ user: 1, createdAt: -1 });
 commentSchema.index({ parentComment: 1 });
 
-// Virtual for like count
-commentSchema.virtual('likeCount').get(function() {
-  return this.likes.length;
+// Virtual for likes count
+commentSchema.virtual('likesCount').get(function() {
+  return this.likes ? this.likes.length : 0;
 });
 
-// Virtual for reply count
-commentSchema.virtual('replyCount', {
-  ref: 'Comment',
-  localField: '_id',
-  foreignField: 'parentComment',
-  count: true
+// Pre-save middleware to set authorName if not provided
+commentSchema.pre('save', async function(next) {
+  if (!this.authorName || this.authorName === 'Anonymous') {
+    try {
+      const User = mongoose.model('User');
+      const user = await User.findById(this.user).select('name username');
+      this.authorName = user?.name || user?.username || 'Anonymous';
+    } catch (e) {
+      // Keep default
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
